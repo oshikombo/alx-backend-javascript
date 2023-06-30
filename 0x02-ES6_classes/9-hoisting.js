b1b284a5169286861f98b7e2a54d86a39df98dd0
Polykarp Amukuhu <amukuhupolykarp@gmail.com>
2023-06-30 08:56:29 +0000

    All TAsks

---
+++ 0x02-ES6_classes/9-hoisting.js
@@ -0,0 +1,33 @@
+class HolbertonClass {
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
+  }
+
+  get year() {
+    return this._year;
+  }
+
+  get location() {
+    return this._location;
+  }
+}
+
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
+class StudentHolberton {
+  constructor(firstName, lastName, holbertonClass) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
+  }
+
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
+
+  get holbertonClass() {
+    return this.holbertonClass;
+  }
+}
---
+++ 0x02-ES6_classes/README.md
@@ -0,0 +1,1 @@
+# This README.md file is for the 0x02-ES6_classes project.
